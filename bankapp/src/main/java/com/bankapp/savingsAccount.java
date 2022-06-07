2940d11ed2310563f701c39c6af71985a9c27966
Nestor Prodigalidad <46760251+NestorProdigalidad@users.noreply.github.com>
2022-06-06 21:37:00 -0400

    Files from local

---
+++ bankapp/src/main/java/com/bankapp/savingsAccount.java
@@ -0,0 +1,12 @@
+package com.bankapp;
+
+public class savingsAccount extends Account{
+    private double interestRate = 0.01;
+    public savingsAccount(){
+        Account acc = new Account(getName());
+        
+
+        
+    }
+
+}
